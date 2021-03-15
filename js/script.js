401892b893f06cf273d9ffc9b6ff15af815d48a7
Ermygut <ermygut@gmail.com>
2021-03-15 17:02:17 +0200

    input error

--- js/script.js
+++ js/script.js
@@ -510,6 +510,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		const input = document.querySelector(selector);
 
 		input.addEventListener('input', () => {
+// проверяем, если ввел NaN тогда обводим красным цветом весь инпут
+			if (input.value.match(/\D/g)) {
+				input.style.border = '1px solid red';
+			} else {
+				input.style.border = 'none';
+
+			}
+
+
 			switch(input.getAttribute('id')) {
 				case 'height': 
 					height = +input.value;
